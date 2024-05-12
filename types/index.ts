f6bfec831a1026323661a82dde32068f4263cd69
artemphys <artemphys@gmail.com>
2024-05-12 12:24:58 +0200

    Implemented search

--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,15 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+interface HomeProps {
+  searchParams: FilterProps;
+}
